c6df6ab6da5ef70cdef3a7807df37903fafe078a
Hack Reactor Students <github@hackreactor.com>
2013-09-04 15:41:04 -0700

    initial commit, images loading from Reddit

--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,15 @@
-angular.module('myApp.controllers', []);
+angular.module('myApp.controllers', [])
+.controller('MainController', ['$scope', '$http', function($scope, $http){
+  $scope.getArticles = function() {
+    $http({
+      method: 'JSONP',
+      url:'http://www.reddit.com/r/all.json?jsonp=JSON_CALLBACK'})
+    .success(function(data, status){
+      $scope.articles = data.data.children;
+    })
+    .error(function(error, status){
+      console.log('error!', error);
+    });
+  };
+  $scope.getArticles();
+}]);
